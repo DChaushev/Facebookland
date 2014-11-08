ea83dda10c01e07a46fdfcf7bee37d6b1460e0f0
Dimitar Chaushev <d.chaushev073@gmail.com>
2014-11-08 12:44:52 +0200

    add texture to Entity

--- Game/Entity.py
+++ Game/Entity.py
@@ -4,16 +4,18 @@ class Entity:
 
     pos = math.Vector2
 
-    def __init__(self):
-        self.pos.x = 0
-        self.pos.y = 0
+    # def __init__(self):
+    #     self.pos.x = 0
+    #     self.pos.y = 0
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, textureHolder, id):
         self.pos.x = x
         self.pos.y = y
+        self.texture = textureHolder.get(id)
 
-    def __init__(self, vector):
+    def __init__(self, vector, textureHolder, id):
         self.pos = vector
+        self.texture = textureHolder.get(id)
 
     def set_pos(self, x, y):
         self.pos.x = x
@@ -21,3 +23,6 @@ class Entity:
 
     def set_pos(self, vector):
         self.pos = vector
+
+    def set_texture(self, textureHolder, id):
+        self.texture = textureHolder.get(id)
