edce3684c0720bcab34d109524c97958c629ff56
gsinekliev <gsinekliev@gmail.com>
2014-11-08 15:15:33 +0200

    Implemented basic functionality for the MainMenu

---
+++ Common.py
@@ -0,0 +1,14 @@
+USE_PYQT = True
+
+FACEBOOK_INFO_SERVER_ADDRESS = ""
+
+class Person:
+    def __init__( self, name ):
+        self.name = name;
+
+    def name(self):
+        return self.name
+
+class Level:
+    def __init__(self):
+        self.name = "Name"
