3da6671730b7f8c33b9b6d56e86a728263b3fcbf
lolotobg <loloto.loloto@gmail.com>
2014-11-09 00:02:57 +0200

    Work on glitches for the monsters

--- Game/Game.py
+++ Game/Game.py
@@ -19,8 +19,8 @@ class Game:
         self.player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT/2, textureHolder, Texture.PLAYER  )
         self.monsters = [Enemy( 0, 0, textureHolder, Texture.ZOMBIE)]
         self.projectiles = []
-        self.tree = Unit()
-        self.air = [Unit()]
+        #self.tree = Unit()
+        #self.air = [Unit()]
         #self.environment = []
 
     def run(self):
@@ -85,8 +85,8 @@ class Game:
                 projectile.update()
                 projectile.render(screen)
 
-            for cloud in self.air:
-                cloud.update()
-                cloud.render(screen)
+            #for cloud in self.air:
+            #    cloud.update()
+             #   cloud.render(screen)
 
             pygame.display.flip()
