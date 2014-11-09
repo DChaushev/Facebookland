2cebee0ad2dae3a630a31ca7b3211da27a69a063
Dimitar Chaushev <d.chaushev073@gmail.com>
2014-11-09 13:22:51 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Game/Game.py
    	Game/Player.py
    	Game/Unit.py

--- Game/Game.py
+++ Game/Game.py
@@ -12,12 +12,10 @@ from Game.Player import Player
 from Game.Enemy import Enemy
 from random import randint
 
-def exit_game():
-    sys.exit()
 
 class Game:
     def __init__(self, level_options):
-        self.level_options = level_options
+        self.levelOptions = level_options
         self.player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT/2, textureHolder, Texture.PLAYER  )
         self.monsters = [Enemy( 0, 0, textureHolder, Texture.ZOMBIE)]
         for i in range(2):
@@ -27,6 +25,10 @@ class Game:
         #self.tree = Unit()
         #self.air = [Unit()]
         #self.environment = []
+
+    def exit(self):
+        #self.exitGame = True
+        pygame.quit()
 
     def background_render(self, bg, screen):
         x = 0
@@ -59,99 +61,115 @@ class Game:
         surf = pygame.transform.smoothscale(screen, scale_size)
         surf = pygame.transform.smoothscale(surf, surf_size)
 
-
         while 1:
             screen.blit(surf, (0,0))
             screen.blit(text, [text_x, text_y])
 
             for event in pygame.event.get():
-                if event.type in [pygame.K_ESCAPE, pygame.QUIT]:
-                    exit_game()
+                if event.type == pygame.QUIT:
+                    self.exit()
+                    return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.exit()
+                        return
 
             pygame.display.update()
 
 
-    def run(self, levelOptions):
+    def handle_bullet(self, bullet, screen):
+        bullet.update()
+
+        if bullet.pos.x < 0 or bullet.pos.x > SCREEN_WIDTH or bullet.pos.y < 0 or bullet.pos.y > SCREEN_HEIGHT:
+            self.projectiles.remove(bullet)
+            return
+
+        for monster in self.monsters:
+            if self.collision_detection(monster, bullet):
+                monster.reduce_health(self.player.damage)
+                if bullet in self.projectiles:
+                    self.projectiles.remove(bullet)
+                print(monster.health)
+        bullet.render( screen )
+
+    def handle_monsters(self, screen):
+        for monster in self.monsters:
+            monster.set_target(self.player)
+            monster.update()
+            monster.render(screen)
+            if monster.health <= 0:
+                if monster in self.monsters:
+                    self.monsters.remove(monster)
+
+
+    def run(self):
         pygame.init()
         screen = pygame.display.set_mode( ( SCREEN_WIDTH, SCREEN_HEIGHT ), pygame.DOUBLEBUF | pygame.HWSURFACE, 32)
         clock = pygame.time.Clock()
-        while True:
+        while 1:
             # Limit frame speed to 50 FPS
             time_passed = clock.tick(50)
 
-            if not self.isOver:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.exit()
+                    return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key in (pygame.K_UP, pygame.K_w):
+                        self.player.add_direction((0, -1))
+                    if event.key in (pygame.K_DOWN, pygame.K_s):
+                        self.player.add_direction((0, 1))
+                    if event.key in (pygame.K_LEFT, pygame.K_a):
+                        self.player.add_direction((-1, 0))
+                    if event.key in (pygame.K_RIGHT, pygame.K_d):
+                        self.player.add_direction((1, 0))
+                    if event.key == pygame.K_ESCAPE:
+                        self.exit()
+                        return
+                    if event.key == pygame.K_SPACE:
+                        self.projectiles.append( self.player.shoot() )
 
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        exit_game()
-                    elif event.type == pygame.KEYDOWN:
-                        if event.key in (pygame.K_UP, pygame.K_w):
-                            self.player.add_direction((0, -1))
-                        if event.key in (pygame.K_DOWN, pygame.K_s):
-                            self.player.add_direction((0, 1))
-                        if event.key in (pygame.K_LEFT, pygame.K_a):
-                            self.player.add_direction((-1, 0))
-                        if event.key in (pygame.K_RIGHT, pygame.K_d):
-                            self.player.add_direction((1, 0))
-                        if event.key == pygame.K_ESCAPE:
-                            exit_game()
-                        if event.key == pygame.K_SPACE:
-                            self.projectiles.append( self.player.shoot() )
+                elif event.type == pygame.KEYUP:
+                    if event.key in (pygame.K_UP, pygame.K_w):
+                        self.player.add_direction((0, 1))
+                    elif event.key in (pygame.K_DOWN, pygame.K_s):
+                        self.player.add_direction((0, -1))
+                    elif event.key in (pygame.K_LEFT, pygame.K_a):
+                        self.player.add_direction((1, 0))
+                    elif event.key in (pygame.K_RIGHT, pygame.K_d):
+                        self.player.add_direction((-1, 0))
 
-                    elif event.type == pygame.KEYUP:
-                        if event.key in (pygame.K_UP, pygame.K_w):
-                            self.player.add_direction((0, 1))
-                        elif event.key in (pygame.K_DOWN, pygame.K_s):
-                            self.player.add_direction((0, -1))
-                        elif event.key in (pygame.K_LEFT, pygame.K_a):
-                            self.player.add_direction((1, 0))
-                        elif event.key in (pygame.K_RIGHT, pygame.K_d):
-                            self.player.add_direction((-1, 0))
+            # Redraw the background
+            screen.fill(BG_COLOR)
+            textureHolder.load(self.levelOptions.enumTexture, self.levelOptions.enumTexture.value)
+            bg = textureHolder.get(self.levelOptions.enumTexture)
 
-                # Redraw the background
-                screen.fill(BG_COLOR)
-                textureHolder.load(levelOptions.enumTexture, levelOptions.enumTexture.value)
-                bg = textureHolder.get(levelOptions.enumTexture)
+            self.background_render(bg, screen)
+            # Update and redraw all creeps
+            #for player in self.players:
+            self.player.update()
+            self.player.render(screen)
 
-                self.background_render(bg, screen)
-                # Update and redraw all creeps
-                #for player in self.players:
-                self.player.update()
-                self.player.render(screen)
+            if len(self.monsters) > 0:
+                self.handle_monsters(screen)
+            else: #TODO: win after killing all 3 waves of zombies
+                self.game_over(screen, "You Win")
+                return
 
-                if len(self.monsters) > 0:
-                    for monster in self.monsters:
-                        monster.set_target(self.player)
-                        monster.update()
-
-                        if self.player.health < 0:
-                            self.isOver = True
-
-                        monster.render(screen)
-                        if monster.health <= 0:
-                            if monster in self.monsters:
-                                self.monsters.remove(monster)
-                else: #TODO: win after killing all 3 waves of zombies
-                    self.game_over(screen, "You Win")
-
-                for bullet in self.projectiles:
-                    bullet.update()
-
-                    for monster in self.monsters:
-                        if self.collision_detection(monster, bullet):
-                            monster.reduce_health(self.player.damage)
-                            if bullet in self.projectiles:
-                                self.projectiles.remove(bullet)
-                            print(monster.health)
-
-
-                    bullet.render( screen )
-
-                #for cloud in self.air:
-                #    cloud.update()
-                 #   cloud.render(screen)
-
-                pygame.display.flip()
-
-            else:
+            if self.player.health < 0:
                 self.game_over(screen, "Game Over")
+                return
+
+            for bullet in self.projectiles:
+                self.handle_bullet(bullet, screen)
+                print(len(self.projectiles))
+
+
+            #for cloud in self.air:
+            #    cloud.update()
+             #   cloud.render(screen)
+
+            pygame.display.flip()
+
+
+
